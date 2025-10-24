0f70410f1067d95287398a2a83810de08c981409
michodev <mikemaggi71@gmail.com>
2025-10-23 22:30:11 -0400

    start frontend

--- server/src/controllers/remittanceController.js
+++ server/src/controllers/remittanceController.js
@@ -1,9 +1,30 @@
-import { createRemittance } from "../services/remittanceService.js";
+import { createRemittance, getRemittancesBySender,getRemittanceById  } from "../services/remittanceService.js";
 
 export const sendRemittance = async (req, res) => {
   try {
     const remittance = await createRemittance(req.user.id, req.body);
     res.status(201).json(remittance);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const getRemittances = async (req, res) => {
+  try {
+    const remittances = await getRemittancesBySender(req.user.id);
+    res.status(200).json(remittances);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const getRemittance = async (req, res) => {
+  try {
+    const remittance = await getRemittanceById(req.user.id, req.params.id);
+    if (!remittance) {
+      return res.status(404).json({ error: "Remittance not found" });
+    }
+    res.status(200).json(remittance);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
--- server/src/routes/remittanceRoutes.js
+++ server/src/routes/remittanceRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { sendRemittance } from "../controllers/remittanceController.js";
+import { getRemittance, getRemittances, sendRemittance } from "../controllers/remittanceController.js";
 import { protect } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
 router.post("/", protect, sendRemittance);
+router.get("/", protect, getRemittances);
+router.get("/:id", protect, getRemittance);
 
 export default router;
